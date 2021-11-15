b023ced7371fbb0b1c385648513154d7555c8731
daxtonparke <daxtonparke@gmail.com>
2021-11-15 16:56:14 -0700

    complete

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -290,13 +290,13 @@ function pond() {
 */
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = []
+let globalScope = ['duck']
 
 // //This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = []
+let bathroomScope = ['rubberduck','duck',]
 
 // //This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = []
+let bathtubScope = ['sailorDuck','rubberduck','duck']
 
 // //This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = []
+let pondScope = ['realDuck','duck']
